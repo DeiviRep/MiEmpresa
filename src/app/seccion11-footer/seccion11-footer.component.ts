72fc39ff917ca6bf15040ad192f6eee849357117
DeiviRep <patzivargasdavid@gmail.com>
2023-03-08 18:03:50 -0400

    V3.4 footer y seccion8

--- src/app/seccion11-footer/seccion11-footer.component.ts
+++ src/app/seccion11-footer/seccion11-footer.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-
+import { faPaperPlane } from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-seccion11-footer',
   templateUrl: './seccion11-footer.component.html',
   styleUrls: ['./seccion11-footer.component.css']
 })
 export class Seccion11FooterComponent {
-
+  send = faPaperPlane;
 }
