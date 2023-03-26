483608e48322e8045ff8538d3c83242b0032d496
DeiviRep <patzivargasdavid@gmail.com>
2023-03-26 19:39:37 -0400

    V5.6 seccion 2

--- src/app/seccion2/seccion2.component.ts
+++ src/app/seccion2/seccion2.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
+// import Swiper core and required modules
+import SwiperCore, { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
+// install Swiper modules
+SwiperCore.use([Navigation, Pagination, Scrollbar, A11y]);
 @Component({
   selector: 'app-seccion2',
   templateUrl: './seccion2.component.html',
-  styleUrls: ['./seccion2.component.css']
+  styleUrls: ['./seccion2.component.css'],
+  encapsulation: ViewEncapsulation.ShadowDom,
 })
 export class Seccion2Component {
+  public img_proyectos:Array<any>;
 
+  constructor(){
+    this.img_proyectos = [
+      './img/3409297.jpg',
+    ]
+  }
 }
